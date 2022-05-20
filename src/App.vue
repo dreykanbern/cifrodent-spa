a97a538515f281d11f4531a6ccc4e15719f579f6
Daniil Kazachkin <dreykanbern@icloud.com>
2022-05-20 20:41:51 +0300

    teeth-map component

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,7 @@
 <template>
   <div class="container">
     <h1 class="container__h1">Заказ-наряд на фрезеровку</h1>
-    <h2 class="container__h2">Карта-зубов</h2>
-
-    <div class="container__teeth-map">
-
-      <div class="teeth-map__first-column">
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-      </div>
-      <div class="teeth-map__second-column">
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-      </div>
-      <div class="teeth-map__third-column">
-        <div class="third-column__item">18</div>
-        <div class="third-column__item">17</div>
-        <div class="third-column__item">16</div>
-        <div class="third-column__item">15</div>
-        <div class="third-column__item">14</div>
-        <div class="third-column__item">13</div>
-        <div class="third-column__item">12</div>
-        <div class="third-column__item">11</div>
-        <div class="third-column__item">21</div>
-        <div class="third-column__item">22</div>
-        <div class="third-column__item">23</div>
-        <div class="third-column__item">24</div>
-        <div class="third-column__item">25</div>
-        <div class="third-column__item">26</div>
-        <div class="third-column__item">27</div>
-        <div class="third-column__item">28</div>
-      </div>
-      
-    </div>
-
-      <!-- Выделить модификатор rotate-180 -->
-    <div class="container__teeth-map rotate-180"> 
-
-      <div class="teeth-map__first-column">
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-        <div class="first-column__item"></div>
-      </div>
-      <div class="teeth-map__second-column">
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-        <div class="second-column__item"></div>
-      </div>
-      <div class="teeth-map__third-column">
-        <div class="third-column__item rotate-180">48</div>
-        <div class="third-column__item rotate-180">47</div>
-        <div class="third-column__item rotate-180">46</div>
-        <div class="third-column__item rotate-180">45</div>
-        <div class="third-column__item rotate-180">44</div>
-        <div class="third-column__item rotate-180">43</div>
-        <div class="third-column__item rotate-180">42</div>
-        <div class="third-column__item rotate-180">41</div>
-        <div class="third-column__item rotate-180">31</div>
-        <div class="third-column__item rotate-180">32</div>
-        <div class="third-column__item rotate-180">33</div>
-        <div class="third-column__item rotate-180">34</div>
-        <div class="third-column__item rotate-180">35</div>
-        <div class="third-column__item rotate-180">36</div>
-        <div class="third-column__item rotate-180">37</div>
-        <div class="third-column__item rotate-180">38</div>
-      </div>
-      
-    </div>
-
+    <TeethMap :teeth1="teeth1" :teeth2="teeth2"/>
     <div class="container__info">
 
       <div class="info__wrapper">
@@ -201,9 +85,50 @@
 <style lang='scss' src="./app.scss"></style>
 
 <script>
+import TeethMap from "@/components/TeethMap/TeethMap";
 export default {
+  components: {
+    TeethMap
+  },
   data() {
-
+    return {
+      teeth1: [
+        {id: 1, value:18},
+        {id: 2, value:17},
+        {id: 3, value:16},
+        {id: 4, value:15},
+        {id: 5, value:14},
+        {id: 6, value:13},
+        {id: 7, value:12},
+        {id: 8, value:11},
+        {id: 9, value:21},
+        {id: 10, value:22},
+        {id: 11, value:23},
+        {id: 12, value:24},
+        {id: 13, value:25},
+        {id: 14, value:26},
+        {id: 15, value:27},
+        {id: 16, value:28},
+      ],
+      teeth2: [
+        {id: 17, value:48},
+        {id: 18, value:47},
+        {id: 19, value:46},
+        {id: 20, value:45},
+        {id: 21, value:44},
+        {id: 22, value:43},
+        {id: 23, value:42},
+        {id: 24, value:41},
+        {id: 25, value:31},
+        {id: 26, value:32},
+        {id: 27, value:33},
+        {id: 28, value:34},
+        {id: 29, value:35},
+        {id: 30, value:36},
+        {id: 31, value:37},
+        {id: 32, value:38},
+      ]
+    }
   },
   methods: {
 
---
+++ src/components/TeethMap/TeethMap.vue
@@ -0,0 +1,121 @@
+<template>
+<div class="teeth-map">
+
+  <h2 class="teeth-map-header">Карта-зубов</h2>
+
+  <div class="teeth-map__wrapper">
+
+    <div class="teeth-map__container">
+
+      <div class="teeth-map__first-column">
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+      </div>
+      <div class="teeth-map__second-column">
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+      </div>
+      <div class="teeth-map__third-column" >
+        <div class="third-column__item" v-for="tooth in teeth1">{{tooth.value}}</div>
+      </div>
+
+    </div>
+
+    <!-- Выделить модификатор rotate-180 -->
+    <div class="teeth-map__container rotate-180">
+
+      <div class="teeth-map__first-column">
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+        <div class="first-column__item"></div>
+      </div>
+      <div class="teeth-map__second-column">
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+        <div class="second-column__item"></div>
+      </div>
+      <div class="teeth-map__third-column">
+        <div class="third-column__item rotate-180"  v-for="tooth in teeth2">{{tooth.value}}</div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+</template>
+
+<style lang='scss' src="./teeth-map.scss"></style>
+
+<script>
+export default {
+  props: {
+    teeth1: {
+      type: Array,
+      required: true
+    },
+    teeth2: {
+      type: Array,
+      required: true
+    },
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
